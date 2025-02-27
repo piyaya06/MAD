a21657d114b3c72570594d7d146f95957b1d86d3
piyaya06 <monoapril7@gmail.com>
2025-02-27 14:07:50 +0800

    flex, justifycontent, alignitems

---
+++ Exercise3.js
@@ -0,0 +1,75 @@
+//Exercise 3 - Sign In
+
+//1. import core component react-native
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+//2. Buat component
+const SignIn = () => {
+  return (
+    //jsx
+    <>
+      <Text style={styles.title}>Welcome</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.username}>Username</Text>
+        <TextInput style={styles.input} placeholder="Masukan username anda" />
+        <Text style={styles.username}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukan password anda"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </>
+  );
+};
+
+//Styling
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 50,
+    fontWeight: '800',
+    color: 'black',
+    padding: 10,
+    margin: 10,
+    textAlign: 'left',
+  },
+  username: {
+    fontSize: 20,
+    marginLeft: 20,
+    fontWeight: '400',
+    color: 'black',
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 2,
+    margin: 10,
+    fontSize: 15,
+    borderRadius: 10,
+    padding: 10,
+    color: 'black',
+  },
+  button: {
+    backgroundColor: 'blue',
+    margin: 10,
+    padding: 10,
+    borderRadius: 15,
+  },
+  buttonText: {
+    fontSize: 15,
+    textAlign: 'center',
+    color: 'white',
+  },
+});
+
+export default SignIn;
---
+++ Flexbox.tsx
@@ -0,0 +1,40 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Flexbox = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.redContainer}></View>
+        <View style={styles.greenContainer}></View>
+        <View style={styles.blueContainer}></View>
+      </View>
+    </>
+  );
+};
+
+export default Flexbox;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row', //utk xy axis//row//column//row-reverse//column-reverse
+    justifyContent: 'space-evenly', //utk main axis//flex-start//flex-end//center//space-between//space-around//space-evenly
+    alignItems: 'center', //cross axis
+  },
+  redContainer: {
+    backgroundColor: 'red',
+    height: 100,
+    width: 100,
+  },
+  greenContainer: {
+    backgroundColor: 'green',
+    height: 100,
+    width: 100,
+  },
+  blueContainer: {
+    backgroundColor: 'blue',
+    height: 100,
+    width: 100,
+  },
+});
