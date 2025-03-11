5c87e2048f71a9da6d05e94578e1804c6faa72c9
piyaya06 <monoapril7@gmail.com>
2025-03-11 14:52:44 +0800

    Exercise 6

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import Exercise3 from './Exercise3';
 import Flexbox from './Flexbox';
 import Exercise4 from './Exercise4';
 import Exercise5 from './Exercise5';
+import Exercise6 from './Exercise6';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Exercise5);
+AppRegistry.registerComponent(appName, () => Exercise6);
